1bb9f93ab73ad9f3b115e73a8a5b0bb966bb9553
CryoYohan <cyrilypil@gmail.com>
2025-07-27 17:44:52 +0800

    Generate CSV for German to English words csv

--- flash-card-project-start/main.py
+++ flash-card-project-start/main.py
@@ -1,2 +1,10 @@
 BACKGROUND_COLOR = "#B1DDC6"
 
+with open("de_50k.txt", "r") as file:
+    data = file.read()
+    print(type(data))
+    german_words_list = data.split("\n")
+
+with open("data/german_words.csv", "a") as new_file:
+    for i in range(100):
+        new_file.write(f"{german_words_list[i].split(" ")[0]}\n")
